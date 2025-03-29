f5f315c4ead3783f07e24298b8f7926d03e1600f
Tahmeed Ferdous <tahmeed.ferdous@g.bracu.ac.bd>
2025-03-30 05:14:51 +0600

    ds

---
+++ README.md
@@ -0,0 +1,2 @@
+# UniCourse
+ An E-Learning platform made for students of computer science in search for algorthims and data structures
---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay];
+
+const navToggleEvent = function (elem) {
+  for (let i = 0; i < elem.length; i++) {
+    elem[i].addEventListener("click", function () {
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
+    });
+  }
+}
+
+navToggleEvent(navElemArr);
+navToggleEvent(navLinks);
+
+
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+});
