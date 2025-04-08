f4ff3fabd4fbb3f56b93e0973b8c32edd709d188
Tahmeed Ferdous <tahmeed.ferdous@g.bracu.ac.bd>
2025-04-08 16:42:40 +0600

    up

--- Student/studentProfile.php
+++ Student/studentProfile.php
@@ -12,4 +12,16 @@ if(isset($_SESSION['is_login'])) {
     echo "<script>location.href='../index.php';</script>";
 }
 
+$sql = "SELECT * FROM student WHERE stu_email = '$stuEmail'";
+$result = $conn->query($sql);
+if($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+    $stu_id = $row['stu_id'];
+    $stu_name = $row['stu_name'];
+    $stu_occ = $row['stu_occ'];
+    $stu_img = $row['img'];
+} else {
+    echo "<script>location.href='../index.php';</script>";
+}
+
 ?>
